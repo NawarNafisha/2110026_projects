931250675b4a51d85f2803dd9c0ab53219b3e3bd
Nafisha Nawar <155055873+NawarNafisha@users.noreply.github.com>
2025-12-05 13:26:38 +0600

    Add files via upload

---
+++ Software Projects/Travel_and_Tour_website_W_adminPanel/build_files/Admin_Dashboard/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('/assets/dashboard.css');?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <title>Control</title>
+</head>
+<body>
+    <header>
+        <!-- Menu Start  -->
+        <menu>
+            <!-- logo  -->
+            <div class="logo_list">
+                <a href="/" class="logo">Control</a>
+                <ul>
+                    <li>
+                        <a href="<?php echo base_url('/dashboard')?>" class="a_active"><i class="bi bi-grid-1x2-fill"></i> Dashboard</a>
+                    </li>
+                    <li>
+                       <a href="<?php echo base_url('/hotels') ?>">
+    <i class="bi bi-buildings-fill"></i> Hotels
+</a>
+
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('/bookings')?>"><i class="bi bi-backpack-fill"></i> Bookings</a>
+                    </li>
+                    <li>
+                         <a href="<?php echo base_url('/users')?>"><i class="bi bi-people-fill"></i> Users</a>
+                    </li>
+                   
+                </ul>
+            </div>
+
+            <!-- Logout Start  -->
+               <!-- <> </>  -->
+            
+          <button><i class="bi bi-power"></i><a href="<?= base_url('/'); ?>" style="color:white; text-decoration:none;">Back</a></button>
+
+
+            
+
+           
+            <!-- Logout End -->
+        </menu>
---
+++ Software Projects/Travel_and_Tour_website_W_adminPanel/build_files/Admin_Dashboard/nav.php
@@ -0,0 +1,13 @@
+ <nav>
+                <div class="left_nav">
+                    <h1>Ayalytics Overview</h1>
+                  
+                </div>
+                <div class="right_nav">
+                  
+                    <div class="profile">
+                     
+                        <h6>Hi <?php echo $this->session->userdata('userdata')['name'];?></h6>
+                    </div>
+                </div>
+            </nav>
